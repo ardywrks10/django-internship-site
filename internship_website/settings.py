c479bc1c141b2dc38813ecbdf42ac50a6294a74f
Ardy Wirakusuma <ardywirakusuma16@gmail.com>
2025-11-20 11:03:34 +0800

    adding whitenoise to production

--- internship_website/settings.py
+++ internship_website/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -94,6 +95,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 LOGIN_URL           = "accounts:login"
 LOGIN_REDIRECT_URL  = "public:index"
