647b1320dab638c0fdfb8845c234519ff0b9a708
Ardy Wirakusuma <ardywirakusuma16@gmail.com>
2025-11-20 21:20:24 +0800

    add: entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+echo ">> Running migrations..."
+python manage.py migrate
+
+echo ">> Collecting static files..."
+python manage.py collectstatic --noinput
+
+echo ">> Starting server..."
+python manage.py runserver 0.0.0.0:$PORT
