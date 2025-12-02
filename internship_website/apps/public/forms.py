646ec05d5c9178eb84ef57dcbee102a6963318c1
Ardy Wirakusuma <ardywirakusuma16@gmail.com>
2025-12-02 22:55:40 +0800

    compressing image & edit image description

--- internship_website/apps/public/forms.py
+++ internship_website/apps/public/forms.py
@@ -5,5 +5,4 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['content']
-
     content = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Write your comment...', 'rows': 4, 'class': 'form-control'}))
